64600fa3de803acbee4b0da024d181bd414c899e
RyanFleming <rflemin3@my.westga.edu>
2016-04-20 18:19:06 -0400

    Added MainActivity tests and scrapped current attempt to test to unit test the DBHandler

--- app/src/androidTest/java/edu/westga/ryanflemingfinalproject/MainActivityTest.java
+++ app/src/androidTest/java/edu/westga/ryanflemingfinalproject/MainActivityTest.java
@@ -1,6 +1,12 @@
 package edu.westga.ryanflemingfinalproject;
 
+import android.inputmethodservice.Keyboard;
+import android.os.SystemClock;
 import android.test.ActivityInstrumentationTestCase2;
+import android.view.KeyEvent;
+import android.widget.Button;
+import android.test.TouchUtils;
+import android.widget.EditText;
 
 import edu.westga.ryanflemingfinalproject.View.MainActivity;
 
@@ -21,7 +27,54 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActiv
         assertNotNull(activity);
     }
 
-    public void testInitialLoadScreen() {
+    /**
+     * Test to make sure button started disableds
+     */
+    public void testInitialLoadScreenSubmitButtonDisabled() {
+        MainActivity activity = getActivity();
+        Button button = (Button) activity.findViewById(R.id.button_submit_user_name);
+        assertFalse(button.isEnabled());
+    }
 
+    /**
+     * Test to make sure button enabled with text
+     */
+    public void testInitialLoadScreenSubmitButtonEnabledWithText() {
+        MainActivity activity = getActivity();
+        Button button = (Button) activity.findViewById(R.id.button_submit_user_name);
+        EditText addUserName = (EditText) activity.findViewById(R.id.add_user_text_edit);
+        this.getFocus(addUserName);
+        this.getInstrumentation().sendStringSync("Test");
+        assertTrue(button.isEnabled());
+        }
+
+    /**
+     * Test to make sure the button is disabled when text added then deleted
+     */
+    public void testInitialLoadScreenSubmitButtonDisabledWhenTextDeleted() {
+        MainActivity activity = getActivity();
+        Button button = (Button) activity.findViewById(R.id.button_submit_user_name);
+        EditText addUserName = (EditText) activity.findViewById(R.id.add_user_text_edit);
+        this.getFocus(addUserName);
+        this.getInstrumentation().sendStringSync("T");
+        this.getInstrumentation().sendCharacterSync(KeyEvent.KEYCODE_DEL);
+
+        assertFalse(button.isEnabled());
+    }
+
+    /**
+     * Gets the focus of an edit text.
+     * @param editText
+     */
+    private void getFocus(EditText editText) {
+        final EditText focusedEditText = editText;
+        getInstrumentation().runOnMainSync(new Runnable() {
+            @Override
+            public void run() {
+                focusedEditText.requestFocus();
+            }
+        });
+        getInstrumentation().waitForIdleSync();
+        SystemClock.sleep(500);
     }
 }
--- app/src/main/java/edu/westga/ryanflemingfinalproject/View/MainActivity.java
+++ app/src/main/java/edu/westga/ryanflemingfinalproject/View/MainActivity.java
@@ -1,12 +1,14 @@
 package edu.westga.ryanflemingfinalproject.View;
 
-import android.content.DialogInterface;
+
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Gravity;
 import android.view.View;
 import android.view.Menu;
@@ -76,10 +78,35 @@ public class MainActivity extends AppCompatActivity {
             addText.setTextSize(30);
             addText.setPadding(0, 300, 0, 50);
             addText.setTextColor(Color.BLACK);
+            addText.setId(R.id.add_text_view);
             final EditText etxUser = new EditText(this);
-
-            Button btnSubmit = new Button(this);
+            etxUser.setId(R.id.add_user_text_edit);
+            final Button btnSubmit = new Button(this);
+            btnSubmit.setEnabled(false);
             btnSubmit.setText("Submit");
+            btnSubmit.setId(R.id.button_submit_user_name);
+            etxUser.addTextChangedListener(new TextWatcher() {
+                @Override
+                public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+                }
+
+                @Override
+                public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                }
+
+                @Override
+                public void afterTextChanged(Editable s) {
+                    if (s.toString().equals("")) {
+                        btnSubmit.setEnabled(false);
+                    } else {
+                        btnSubmit.setEnabled(true);
+                    }
+                }
+            });
+
+
 
             btnSubmit.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -120,7 +147,5 @@ public class MainActivity extends AppCompatActivity {
         this.mainActivityLayout.addView(btnAddExpense);
         this.mainActivityLayout.addView(btnAddGoal);
         this.mainActivityLayout.addView(btnCalculate);
-
-
     }
 }
