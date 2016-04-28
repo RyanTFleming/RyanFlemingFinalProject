813269bdce47e264024a8c5237d37390bbf77582
RyanFleming <rflemin3@my.westga.edu>
2016-04-27 20:33:01 -0400

    Added calculations to the results activity and also did some formatting to make i tlook better

--- app/src/main/java/edu/westga/ryanflemingfinalproject/View/ResultsActivity.java
+++ app/src/main/java/edu/westga/ryanflemingfinalproject/View/ResultsActivity.java
@@ -1,6 +1,7 @@
 package edu.westga.ryanflemingfinalproject.View;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,14 +13,23 @@ import android.widget.TextView;
 import org.w3c.dom.Text;
 
 import edu.westga.ryanflemingfinalproject.Controller.DBController;
+import edu.westga.ryanflemingfinalproject.Model.GoalCalculator;
 import edu.westga.ryanflemingfinalproject.R;
 
 public class ResultsActivity extends AppCompatActivity {
 
     private DBController controller;
+    private GoalCalculator calc;
+    private double totalExpenses;
     private double wage;
     private double hours;
+    private double goalValue;
     private String goal;
+
+    private double totalEarnings;
+    private double netEarnings;
+    private double savedPerHour;
+    private double hoursToGoal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,37 +38,67 @@ public class ResultsActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Help", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
         this.controller = new DBController(this, DBController.DATABASE_NAME);
+
 
         Intent intent = this.getIntent();
         this.wage = intent.getDoubleExtra(CalculatorActivity.WAGE_SELECTION, 0.00);
         this.hours = intent.getDoubleExtra(CalculatorActivity.HOURS_SELECTION, 0.00);
         this.goal = intent.getStringExtra(CalculatorActivity.GOAL_SELECTION);
-        double goalValue = this.controller.getGoalValue(this.goal);
-        double expenseTotal = this.controller.getTotalExpenses();
+        this.goalValue = this.controller.getGoalValue(this.goal);
+        this.totalExpenses = this.controller.getTotalExpenses();
+        this.calc = new GoalCalculator(this.wage, this.hours);
+        this.populateResultsFrame();
 
         TextView wageView = (TextView) this.findViewById(R.id.textViewWage);
         TextView hoursView = (TextView) this.findViewById(R.id.textViewHours);
-        TextView goalView = (TextView) this.findViewById(R.id.textViewGoal);
+
         TextView goalValueView = (TextView) this.findViewById(R.id.textViewGoalValue);
         TextView totalExpenseView = (TextView) this.findViewById(R.id.textViewExpenseTotal);
 
 
-        wageView.setText(String.format("$%.2f", this.wage));
-        hoursView.setText(String.format("%.2f", this.hours));
-        goalView.setText(this.goal);
-        goalValueView.setText(String.format("$%.2f", goalValue));
-        totalExpenseView.setText(String.format("Monthly Expenses:$%.2f", expenseTotal));
+        wageView.setText(String.format("Wage:                                 $%.2f", this.wage));
+        hoursView.setText(String.format("Hours                                   %.2f", this.hours));
+        goalValueView.setText(String.format("%s:    $%.2f", this.goal, this.goalValue));
+        totalExpenseView.setText(String.format("Monthly Expenses:     $%.2f", this.totalExpenses));
 
+    }
+
+    private void populateResultsFrame() {
+        TextView totalIncomeView = (TextView) this.findViewById(R.id.totalIncomeTextView);
+        TextView netIncomeView = (TextView) this.findViewById(R.id.netTextView);
+        TextView dollarsPerHourSavedView = (TextView) this.findViewById(R.id.netPerHourTextView);
+        TextView hoursToGoalView = (TextView) this.findViewById(R.id.hrsToGoalTextView);
+        this.totalEarnings = this.calc.monthlyGrossEarning();
+        this.netEarnings = this.calc.monthlyNetEarnings(this.totalExpenses);
+        this.savedPerHour = this.calc.amountSavedPerHour(this.totalExpenses);
+        this.hoursToGoal = this.calc.hoursNeededToGoal(this.totalExpenses, this.goalValue);
+
+
+
+        totalIncomeView.setText(String.format("$%.2f", this.totalEarnings));
+
+
+        netIncomeView.setText(String.format("$%.2f", this.netEarnings));
+        if (this.netEarnings < 0) {
+            netIncomeView.setTextColor(Color.RED);
+        } else {
+            netIncomeView.setTextColor(Color.BLACK);
+        }
+
+        dollarsPerHourSavedView.setText(String.format("$%.2f", this.savedPerHour));
+        if (this.savedPerHour < 0) {
+            dollarsPerHourSavedView.setTextColor(Color.RED);
+        } else {
+            dollarsPerHourSavedView.setTextColor(Color.BLACK);
+        }
+
+        if (this.hoursToGoal < 0) {
+            hoursToGoalView.setText("Unachievable");
+            hoursToGoalView.setTextColor(Color.RED);
+        } else {
+            hoursToGoalView.setTextColor(Color.BLACK);
+            hoursToGoalView.setText(String.format("%.2f", this.hoursToGoal));
+        }
     }
 }
